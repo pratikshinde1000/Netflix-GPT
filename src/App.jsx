3fb9ab94f0878fdc7235edf39b498ad8539b27be
Pratik Shinde <pratikshinde1000@.com>
2025-06-18 11:18:13 +0530

    Basic Code

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+function App() {
+
+  return (
+    <>
+      <div className='text-red-500 text-3xl font-bold underline'>
+        Create React App
+      </div>
+    </>
+  )
+}
+
+export default App
